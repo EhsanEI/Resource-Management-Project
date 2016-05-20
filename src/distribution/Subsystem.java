b1a033269759b3222a62bf8176d0de5bcb219e9a
Ehsan <ehsan.silverman@gmail.com>
2016-05-21 01:43:38 +0430

    added constructors

--- src/distribution/Subsystem.java
+++ src/distribution/Subsystem.java
@@ -2,7 +2,9 @@ package distribution;
 
 public class Subsystem extends InformationResource {
 
-	private String name;
 	private Module[] modules;
 
+	public Subsystem(String name, Requirement[] requirements, int id) {
+		super(name, requirements, id);
+	}
 }
