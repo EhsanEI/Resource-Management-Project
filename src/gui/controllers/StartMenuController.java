f47614cb6f48a591e4ff85b0f3c1b611e6627773
qizilbash <m.shafiei0423@gmail.com>
2016-07-01 21:35:27 -0700

    Gui Updated

--- src/gui/controllers/StartMenuController.java
+++ src/gui/controllers/StartMenuController.java
@@ -6,12 +6,15 @@ package gui.controllers;
 import businesslogic.ClientAccountingLogicFacade;
 import gui.MainMenu;
 import javafx.animation.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -25,12 +28,18 @@ public class StartMenuController{
 
     private ClientAccountingLogicFacade clientAccountingLogicFacade;
 
-    @FXML private Pane mainPane;
+    private AnchorPane onTheTopPane;
+    @FXML private AnchorPane signinPane;
+    @FXML private AnchorPane signupPane;
+
     @FXML private ImageView backGroundImage;
 
     @FXML private Button signInButton;
     @FXML private Button SignUpButton;
     @FXML private Button recoverPasswordButton;
+
+    @FXML private Button registerButton;
+    @FXML private Button backFromSignupButton;
 
 
     @FXML private TextField usernameTextField;
@@ -40,22 +49,22 @@ public class StartMenuController{
     @FXML private Label usernameLabel;
 
     private Stage stage;
-    private Button backButton;
 
-    private TextField passwordTextField;
-    private TextField confirmPasswordTextField;
-    private Label confirmPasswordLabel;
+
 
     private Timeline timeline;
 
     private double animationTime = 700;
 
     @FXML
-    private void initialize() throws IOException, ClassNotFoundException {
+    private void
+    initialize() throws IOException, ClassNotFoundException {
         clientAccountingLogicFacade = ClientAccountingLogicFacade.getInstance();
+        onTheTopPane = signinPane;
     }
 
-    @FXML private void signInPressed() throws Exception {
+    @FXML
+    private void signInPressed() throws Exception {
         System.out.println(usernameTextField.getText() + "@" + passwordField.getText());
         stage = (Stage) usernameTextField.getScene().getWindow();
         stage.close();
@@ -67,243 +76,64 @@ public class StartMenuController{
     }
 
 
-    @FXML private void signUpPressed() {
-        System.out.println("sign up pressed");
-        changePane2SignUp();
-    }
-
-    @FXML private void recoverPasswordPressed() throws IOException {
-        System.out.println(clientAccountingLogicFacade.recoverPassword("me").getContent());
-    }
-
-    private void changePane2SignIn(){
-
-    }
-
-    private void register(){
-        System.out.println("register pressed");
-    }
-
-    private void backTostartMenu(){
-        System.out.println("back pressed");
-
-        backButton.setDisable(true);
-        backButton.setVisible(false);
-
-
-
-
-        signInButton.setDisable(false);
-        FadeTransition fadeTransition = new FadeTransition(Duration.millis(animationTime), signInButton);
-        fadeTransition.setFromValue(0);
-        fadeTransition.setToValue(1.0);
-        fadeTransition.setCycleCount(1);
-        fadeTransition.setAutoReverse(false);
-        fadeTransition.play();
-
-        recoverPasswordButton.setDisable(false);
-        FadeTransition fadeTransition1 = new FadeTransition(Duration.millis(animationTime), recoverPasswordButton);
-        fadeTransition1.setFromValue(0);
-        fadeTransition1.setToValue(1.0);
-        fadeTransition1.setAutoReverse(true);
-        fadeTransition1.setCycleCount(1);
-        fadeTransition1.play();
-
-        passwordTextField.setDisable(true);
-
-        FadeTransition fadeTransition2 = new FadeTransition(Duration.millis(animationTime), passwordTextField);
-        fadeTransition2.setFromValue(1.0);
-        fadeTransition2.setToValue(0);
-        fadeTransition2.setAutoReverse(true);
-        fadeTransition2.setCycleCount(1);
-        fadeTransition2.play();
-
-        confirmPasswordTextField.setDisable(true);
-        FadeTransition fadeTransition3 = new FadeTransition(Duration.millis(animationTime), confirmPasswordTextField);
-        fadeTransition3.setFromValue(1.0);
-        fadeTransition3.setToValue(0);
-        fadeTransition3.setAutoReverse(true);
-        fadeTransition3.setCycleCount(1);
-        fadeTransition3.play();
-
-        confirmPasswordLabel.setDisable(true);
-        FadeTransition fadeTransition4 = new FadeTransition(Duration.millis(animationTime), confirmPasswordLabel);
-        fadeTransition4.setFromValue(1.0);
-        fadeTransition4.setToValue(0);
-        fadeTransition4.setAutoReverse(true);
-        fadeTransition4.setCycleCount(1);
-        fadeTransition4.play();
-
-        passwordField.setDisable(false);
-        passwordField.setVisible(true);
-        passwordField.setLayoutX(usernameTextField.getLayoutX());
-        passwordField.toFront();
-
-
-        timeline = new Timeline();
-
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(usernameTextField.layoutXProperty(),
-                                usernameTextField.getLayoutX() - 80,
-                                Interpolator.EASE_BOTH)));
-
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(SignUpButton.layoutXProperty(),
-                                recoverPasswordButton.getLayoutX() + 135,
-                                Interpolator.EASE_BOTH)));
-
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(SignUpButton.layoutYProperty(),
-                                recoverPasswordButton.getLayoutY(),
-                                Interpolator.EASE_BOTH)));
-
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(passwordField.layoutXProperty(),
-                                usernameTextField.getLayoutX() - 80,
-                                Interpolator.EASE_BOTH)));
-
-
-
-        timeline.setCycleCount(1);
-        timeline.setAutoReverse(false);
-        timeline.play();
-
-        timeline.setOnFinished(event -> {
-            stage.setWidth(410);
-            stage.setHeight(320);
-            backGroundImage.setFitHeight(320);
-            backGroundImage.setFitWidth(410);
-
-            SignUpButton.setText("Sign up");
-
-            SignUpButton.setOnAction(e -> {
-                Object source = e.getSource();
-                if (source instanceof Button) {
-                    signUpPressed();
-                }
-            });
-
-        });
-    }
-
-    private void changePane2SignUp(){
-
+    @FXML
+    private void signUpPressed() {
         stage = (Stage) usernameTextField.getScene().getWindow();
-        stage.setWidth(490);
-        stage.setHeight(500);
+        System.out.println("sign up pressed");
+        backGroundImage.setFitHeight(620);
+        stage.setHeight(620);
+        animatePaneChange(signupPane, true);
+    }
 
-        backGroundImage.setFitHeight(500);
-        backGroundImage.setFitWidth(490);
-
-
-        passwordTextField = new TextField();
-        passwordTextField.setEffect(passwordField.getEffect());
-        passwordTextField.setLayoutX(passwordField.getLayoutX());
-        passwordTextField.setLayoutY(passwordField.getLayoutY());
-        passwordTextField.setPrefWidth(passwordField.getPrefWidth());
-
-        confirmPasswordTextField = new TextField();
-        confirmPasswordTextField.setEffect(passwordField.getEffect());
-        confirmPasswordTextField.setLayoutX(passwordField.getLayoutX());
-        confirmPasswordTextField.setLayoutY(passwordField.getLayoutY() + (passwordField.getLayoutY()- usernameTextField.getLayoutY()));
-        confirmPasswordTextField.setPrefWidth(passwordField.getPrefWidth());
+    @FXML
+    private void recoverPasswordPressed() throws IOException {
+        System.out.println(clientAccountingLogicFacade.recoverPassword(usernameTextField.getText()).getContent());
+    }
 
 
-        confirmPasswordLabel = new Label("Confirm Password:");
-        confirmPasswordLabel.setEffect(passwordLabel.getEffect());
-        confirmPasswordLabel.setFont(passwordLabel.getFont());
-        confirmPasswordLabel.setLayoutX(-100);
-        confirmPasswordLabel.setLayoutY(passwordLabel.getLayoutY() + (passwordLabel.getLayoutY() - usernameLabel.getLayoutY() ) + 15);
+    public void backFromSignupButtonPressed(ActionEvent event) {
+        stage.setHeight(320);
+        backGroundImage.setFitHeight(320);
+        animatePaneChange(signinPane,false);
+    }
 
-        backButton = new Button("Back");
-        backButton.setEffect(SignUpButton.getEffect());
-        backButton.setLayoutX(SignUpButton.getLayoutX() + 20);
-        backButton.setLayoutY(SignUpButton.getLayoutY() + 180);
-        backButton.setStyle(SignUpButton.getStyle());
-        backButton.setFont(SignUpButton.getFont());
-        backButton.setOnAction(event1 -> backTostartMenu());
+    public void registerButtonPressed(ActionEvent event) {
+    }
 
-        mainPane.getChildren().addAll(backButton, confirmPasswordLabel,confirmPasswordTextField,passwordTextField);
+    public void animatePaneChange(AnchorPane anchorPane, boolean direction){
 
-        SignUpButton.setText("Register");
+        double yPos1 = -600;
+        double yPos2 = 600;
 
-        SignUpButton.setOnAction(e -> {
-            Object source = e.getSource();
-            if (source instanceof Button) {
-                register();
-            }
-        });
+        if (!direction){
+            yPos1 = 600;
+            yPos2 = -600;
+        }
 
-        timeline = new Timeline();
+        anchorPane.setLayoutX(0);
+        anchorPane.setLayoutY(yPos1);
 
+        Timeline timeline = new Timeline();
 
         timeline.getKeyFrames().add(
                 new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(usernameTextField.layoutXProperty(),
-                                usernameTextField.getLayoutX() + 80,
+                        new KeyValue(anchorPane.layoutYProperty(),
+                                0,
                                 Interpolator.EASE_BOTH)));
-
         timeline.getKeyFrames().add(
                 new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(passwordTextField.layoutXProperty(),
-                                usernameTextField.getLayoutX() + 80,
+                        new KeyValue(onTheTopPane.layoutYProperty(),
+                                yPos2,
                                 Interpolator.EASE_BOTH)));
 
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(confirmPasswordTextField.layoutXProperty(),
-                                usernameTextField.getLayoutX() + 80,
-                                Interpolator.EASE_BOTH)));
-
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(confirmPasswordLabel.layoutXProperty(),
-                                passwordLabel.getLayoutX(),
-                                Interpolator.EASE_BOTH)));
-
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(SignUpButton.layoutXProperty(),
-                                passwordTextField.getLayoutX()+ 210,
-                                Interpolator.EASE_BOTH)));
-
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(animationTime),
-                        new KeyValue(SignUpButton.layoutYProperty(),
-                                backButton.getLayoutY(),
-                                Interpolator.EASE_BOTH)));
 
 
         timeline.setCycleCount(1);
         timeline.setAutoReverse(false);
         timeline.play();
 
-
-
-
-        signInButton.setDisable(true);
-        FadeTransition fadeTransition = new FadeTransition(Duration.millis(animationTime), signInButton);
-        fadeTransition.setFromValue(1.0);
-        fadeTransition.setToValue(0);
-        fadeTransition.setCycleCount(1);
-        fadeTransition.setAutoReverse(false);
-        fadeTransition.play();
-
-        recoverPasswordButton.setDisable(true);
-        FadeTransition fadeTransition1 = new FadeTransition(Duration.millis(700), recoverPasswordButton);
-        fadeTransition1.setFromValue(1.0);
-        fadeTransition1.setToValue(0);
-        fadeTransition1.setAutoReverse(true);
-        fadeTransition1.setCycleCount(1);
-        fadeTransition1.play();
-
-        passwordField.setDisable(true);
-        passwordField.setVisible(false);
-
+        timeline.setOnFinished(event -> onTheTopPane = anchorPane);
     }
+
 
 }
