1785347926e6c1e30390a3cfc5102fe832006f99
qizilbash <m.shafiei0423@gmail.com>
2016-07-09 23:14:52 -0700

    Diagrams Done

--- src/gui/controllers/resourcemanager/ResourceRequirementsReportView.java
+++ src/gui/controllers/resourcemanager/ResourceRequirementsReportView.java
@@ -18,6 +18,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
@@ -29,6 +32,20 @@ import java.util.Optional;
  * Created by qizilbash on 7/4/2016.
  */
 public class ResourceRequirementsReportView extends Controller {
+
+    @FXML private AnchorPane reqPane1;
+    @FXML private AnchorPane reqPane2;
+    @FXML private AnchorPane reqPane3;
+
+    @FXML private Label startLabel;
+    @FXML private Label endLabel;
+    @FXML private Rectangle sampleRect;
+
+    @FXML private Label reqLabel1;
+    @FXML private Label reqLabel2;
+    @FXML private Label reqLabel3;
+
+
 
     @FXML private AnchorPane resourceRequirementReportPane;
     @FXML private AnchorPane diagramPane;
@@ -105,9 +122,128 @@ public class ResourceRequirementsReportView extends Controller {
         return informationResource.getID() + informationResource.getName();
     }
 
+    private int getdays(String date){
+        String[] parts = date.split("/");
+        return Integer.parseInt(parts[2]) *365 + Integer.parseInt(parts[1])*30 + Integer.parseInt(parts[0]);
+    }
 
+    private Rectangle copyRect(Rectangle rectangle,double x, double w){
+        Rectangle rect = new Rectangle();
+        rect.setLayoutX(x+200);
+        rect.setLayoutY(20);
+        rect.setWidth(w);
+        rect.setHeight(rectangle.getHeight());
+        rect.setEffect(rectangle.getEffect());
+        rect.setArcWidth(rectangle.getArcWidth());
+        rect.setArcHeight(rectangle.getArcHeight());
+        return rect;
+    }
 
     private void plot() {
+
+
+        String[][] contents = new String[2][5];
+        contents[0][0] = "PC";
+        contents[0][1] = "5";
+        contents[0][2] = "22/06/12";
+        contents[0][3] = "28/06/12";
+        contents[0][4] = "Essential";
+
+        contents[1][0] = "Printer";
+        contents[1][1] = "5";
+        contents[1][2] = "2/07/12";
+        contents[1][3] = "9/07/12";
+        contents[1][4] = "Ordinary";
+
+
+        if(contents.length <3)
+            reqPane3.setVisible(false);
+        if(contents.length<2)
+            reqPane2.setVisible(false);
+        if (contents.length<1)
+            reqPane1.setVisible(false);
+
+        int reqNum = contents.length;
+        if (reqNum > 3 )
+            reqNum = 3;
+
+
+
+            int[][] lengths = new int[reqNum][2];
+
+            for (int i = 0; i < reqNum; i++) {
+                lengths[i][0] = getdays(contents[i][2]);
+                lengths[i][1] = getdays(contents[i][3]);
+
+            }
+
+            double min = Integer.MAX_VALUE;
+
+            double max = Integer.MIN_VALUE;
+
+            for (int i = 0; i < lengths.length; i++) {
+                if (lengths[i][1] > max) {
+                    max = lengths[i][1];
+                    endLabel.setText(contents[i][3]);
+                }
+                if (lengths[i][0] < min) {
+                    min = lengths[i][0];
+                    startLabel.setText(contents[i][2]);
+                }
+            }
+
+
+            max -= min;
+            System.out.println("max = " + max);
+
+            for (int i = 0; i < lengths.length; i++) {
+                lengths[i][0] -= min;
+                lengths[i][1] -= min;
+
+                double x = (lengths[i][0] / max) * 500.0;
+                double y = ((lengths[i][1] - lengths[i][0]) / max) * 500.0;
+
+
+                System.out.println(y + " : " + x);
+
+                Rectangle rectangle = copyRect(sampleRect, x, y);
+
+                Text text = new Text(contents[i][0]);
+                text.setStyle(startLabel.getStyle());
+                text.setFill(Color.WHITE);
+                text.setFont(startLabel.getFont());
+                text.setLayoutX(200 + x + y/2 - text.getLayoutBounds().getWidth()/2);
+                text.setLayoutY(35);
+
+                int daysCount = (lengths[i][1] - lengths[i][0]);
+                Text days = new Text(String.valueOf(daysCount));
+                days.setStyle(startLabel.getStyle());
+                days.setFont(startLabel.getFont());
+                days.setFill(startLabel.getTextFill());
+                days.setEffect(startLabel.getEffect());
+                days.setLayoutX(200 + x + y/2 - days.getLayoutBounds().getWidth()/2);
+                days.setLayoutY(15);
+
+
+
+                switch (i){
+                    case 0:
+                        reqPane1.getChildren().addAll(rectangle, text, days);
+                        reqLabel1.setText("Requirement 1");
+                        break;
+                    case 1:
+                        reqPane2.getChildren().addAll(rectangle, text, days);
+                        reqLabel2.setText("Requirement 2");
+                        break;
+                    case 2:
+                        reqPane3.getChildren().addAll(rectangle, text, days);
+                        reqLabel3.setText("Requirement 3");
+                        break;
+                }
+
+
+            }
+
 
     }
 
